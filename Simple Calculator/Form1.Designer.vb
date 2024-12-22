090c6f7c58503a5049e9ea2092c4b9be737fd424
dbynarie <72026143+dbynarie@users.noreply.github.com>
2024-12-21 20:41:20 -0800

    update from deni rismanto

---
+++ Simple Calculator/Form1.Designer.vb
@@ -0,0 +1,263 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        txtDisplay = New TextBox()
+        btn7 = New Button()
+        btn8 = New Button()
+        btnClear = New Button()
+        btn9 = New Button()
+        btn4 = New Button()
+        btn6 = New Button()
+        btn5 = New Button()
+        btnDivide = New Button()
+        btn1 = New Button()
+        btn3 = New Button()
+        btn2 = New Button()
+        btnMultiply = New Button()
+        btnPlus = New Button()
+        btn0 = New Button()
+        btnMinus = New Button()
+        btnEqual = New Button()
+        SuspendLayout()
+        ' 
+        ' txtDisplay
+        ' 
+        txtDisplay.Location = New Point(315, 228)
+        txtDisplay.Margin = New Padding(4, 5, 4, 5)
+        txtDisplay.Name = "txtDisplay"
+        txtDisplay.ReadOnly = True
+        txtDisplay.Size = New Size(634, 34)
+        txtDisplay.TabIndex = 0
+        txtDisplay.Text = "0"
+        txtDisplay.TextAlign = HorizontalAlignment.Center
+        ' 
+        ' btn7
+        ' 
+        btn7.Location = New Point(315, 316)
+        btn7.Margin = New Padding(4, 5, 4, 5)
+        btn7.Name = "btn7"
+        btn7.Size = New Size(118, 43)
+        btn7.TabIndex = 1
+        btn7.Text = "7"
+        btn7.UseVisualStyleBackColor = True
+        ' 
+        ' btn8
+        ' 
+        btn8.Location = New Point(485, 316)
+        btn8.Margin = New Padding(4, 5, 4, 5)
+        btn8.Name = "btn8"
+        btn8.Size = New Size(118, 43)
+        btn8.TabIndex = 2
+        btn8.Text = "8"
+        btn8.UseVisualStyleBackColor = True
+        ' 
+        ' btnClear
+        ' 
+        btnClear.Location = New Point(833, 316)
+        btnClear.Margin = New Padding(4, 5, 4, 5)
+        btnClear.Name = "btnClear"
+        btnClear.Size = New Size(118, 43)
+        btnClear.TabIndex = 3
+        btnClear.Text = "Clear"
+        btnClear.UseVisualStyleBackColor = True
+        ' 
+        ' btn9
+        ' 
+        btn9.Location = New Point(660, 316)
+        btn9.Margin = New Padding(4, 5, 4, 5)
+        btn9.Name = "btn9"
+        btn9.Size = New Size(118, 43)
+        btn9.TabIndex = 4
+        btn9.Text = "9"
+        btn9.UseVisualStyleBackColor = True
+        ' 
+        ' btn4
+        ' 
+        btn4.Location = New Point(315, 387)
+        btn4.Margin = New Padding(4, 5, 4, 5)
+        btn4.Name = "btn4"
+        btn4.Size = New Size(118, 43)
+        btn4.TabIndex = 5
+        btn4.Text = "4"
+        btn4.UseVisualStyleBackColor = True
+        ' 
+        ' btn6
+        ' 
+        btn6.Location = New Point(660, 387)
+        btn6.Margin = New Padding(4, 5, 4, 5)
+        btn6.Name = "btn6"
+        btn6.Size = New Size(118, 43)
+        btn6.TabIndex = 6
+        btn6.Text = "6"
+        btn6.UseVisualStyleBackColor = True
+        ' 
+        ' btn5
+        ' 
+        btn5.Location = New Point(485, 387)
+        btn5.Margin = New Padding(4, 5, 4, 5)
+        btn5.Name = "btn5"
+        btn5.Size = New Size(118, 43)
+        btn5.TabIndex = 7
+        btn5.Text = "5"
+        btn5.UseVisualStyleBackColor = True
+        ' 
+        ' btnDivide
+        ' 
+        btnDivide.Location = New Point(833, 387)
+        btnDivide.Margin = New Padding(4, 5, 4, 5)
+        btnDivide.Name = "btnDivide"
+        btnDivide.Size = New Size(118, 43)
+        btnDivide.TabIndex = 8
+        btnDivide.Text = "/"
+        btnDivide.UseVisualStyleBackColor = True
+        ' 
+        ' btn1
+        ' 
+        btn1.Location = New Point(315, 463)
+        btn1.Margin = New Padding(4, 5, 4, 5)
+        btn1.Name = "btn1"
+        btn1.Size = New Size(118, 43)
+        btn1.TabIndex = 9
+        btn1.Text = "1"
+        btn1.UseVisualStyleBackColor = True
+        ' 
+        ' btn3
+        ' 
+        btn3.Location = New Point(660, 463)
+        btn3.Margin = New Padding(4, 5, 4, 5)
+        btn3.Name = "btn3"
+        btn3.Size = New Size(118, 43)
+        btn3.TabIndex = 10
+        btn3.Text = "3"
+        btn3.UseVisualStyleBackColor = True
+        ' 
+        ' btn2
+        ' 
+        btn2.Location = New Point(485, 463)
+        btn2.Margin = New Padding(4, 5, 4, 5)
+        btn2.Name = "btn2"
+        btn2.Size = New Size(118, 43)
+        btn2.TabIndex = 11
+        btn2.Text = "2"
+        btn2.UseVisualStyleBackColor = True
+        ' 
+        ' btnMultiply
+        ' 
+        btnMultiply.Location = New Point(833, 463)
+        btnMultiply.Margin = New Padding(4, 5, 4, 5)
+        btnMultiply.Name = "btnMultiply"
+        btnMultiply.Size = New Size(118, 43)
+        btnMultiply.TabIndex = 12
+        btnMultiply.Text = "*"
+        btnMultiply.UseVisualStyleBackColor = True
+        ' 
+        ' btnPlus
+        ' 
+        btnPlus.Location = New Point(315, 540)
+        btnPlus.Margin = New Padding(4, 5, 4, 5)
+        btnPlus.Name = "btnPlus"
+        btnPlus.Size = New Size(118, 43)
+        btnPlus.TabIndex = 13
+        btnPlus.Text = "+"
+        btnPlus.UseVisualStyleBackColor = True
+        ' 
+        ' btn0
+        ' 
+        btn0.Location = New Point(485, 540)
+        btn0.Margin = New Padding(4, 5, 4, 5)
+        btn0.Name = "btn0"
+        btn0.Size = New Size(118, 43)
+        btn0.TabIndex = 14
+        btn0.Text = "0"
+        btn0.UseVisualStyleBackColor = True
+        ' 
+        ' btnMinus
+        ' 
+        btnMinus.Location = New Point(660, 540)
+        btnMinus.Margin = New Padding(4, 5, 4, 5)
+        btnMinus.Name = "btnMinus"
+        btnMinus.Size = New Size(118, 43)
+        btnMinus.TabIndex = 15
+        btnMinus.Text = "-"
+        btnMinus.UseVisualStyleBackColor = True
+        ' 
+        ' btnEqual
+        ' 
+        btnEqual.Location = New Point(833, 540)
+        btnEqual.Margin = New Padding(4, 5, 4, 5)
+        btnEqual.Name = "btnEqual"
+        btnEqual.Size = New Size(118, 43)
+        btnEqual.TabIndex = 16
+        btnEqual.Text = "="
+        btnEqual.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(11F, 28F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(1081, 615)
+        Controls.Add(btnEqual)
+        Controls.Add(btnMinus)
+        Controls.Add(btn0)
+        Controls.Add(btnPlus)
+        Controls.Add(btnMultiply)
+        Controls.Add(btn2)
+        Controls.Add(btn3)
+        Controls.Add(btn1)
+        Controls.Add(btnDivide)
+        Controls.Add(btn5)
+        Controls.Add(btn6)
+        Controls.Add(btn4)
+        Controls.Add(btn9)
+        Controls.Add(btnClear)
+        Controls.Add(btn8)
+        Controls.Add(btn7)
+        Controls.Add(txtDisplay)
+        Font = New Font("Segoe UI", 15F)
+        Margin = New Padding(4, 5, 4, 5)
+        Name = "Form1"
+        Text = "form"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents txtDisplay As TextBox
+    Friend WithEvents btn7 As Button
+    Friend WithEvents btn8 As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btn9 As Button
+    Friend WithEvents btn4 As Button
+    Friend WithEvents btn6 As Button
+    Friend WithEvents btn5 As Button
+    Friend WithEvents btnDivide As Button
+    Friend WithEvents btn1 As Button
+    Friend WithEvents btn3 As Button
+    Friend WithEvents btn2 As Button
+    Friend WithEvents btnMultiply As Button
+    Friend WithEvents btnPlus As Button
+    Friend WithEvents btn0 As Button
+    Friend WithEvents btnMinus As Button
+    Friend WithEvents btnEqual As Button
+
+End Class
---
+++ Simple Calculator/Form1.vb
@@ -0,0 +1,97 @@
+﻿Public Class Form1
+    Dim nilaiPertama As Integer = 0
+    Dim operatorMatematika As String = ""
+    Dim operasiDilakukan As Boolean = False
+
+    ' Ketika tombol angka ditekan
+    Private Sub btnAngka_Click(sender As Object, e As EventArgs) Handles btn0.Click, btn1.Click, btn2.Click, btn3.Click, btn4.Click, btn5.Click, btn6.Click, btn7.Click, btn8.Click, btn9.Click
+        Dim tombol As Button = CType(sender, Button)
+
+        '       If txtDisplay.Text = "0" Or operasiDilakukan Then
+        '      txtDisplay.Text = ""
+        '     operasiDilakukan = False
+        '    End If
+
+        If operasiDilakukan Then
+            txtDisplay.Text &= tombol.Text
+        Else
+            If txtDisplay.Text = "0" Then
+                txtDisplay.Text = tombol.Text
+            Else
+                txtDisplay.Text &= tombol.Text
+            End If
+        End If
+    End Sub
+
+    ' Ketika tombol operasi ditekan
+    Private Sub btnOperator_Click(sender As Object, e As EventArgs) Handles btnPlus.Click, btnMinus.Click, btnMultiply.Click, btnDivide.Click
+        Dim tombol As Button = CType(sender, Button)
+
+        nilaiPertama = Integer.Parse(txtDisplay.Text)
+        operatorMatematika = tombol.Text
+        operasiDilakukan = True
+
+
+        ' Tampilkan operator di txtDisplay
+        txtDisplay.Text &= " " & operatorMatematika & " "
+    End Sub
+
+    ' Ketika tombol hasil (=) ditekan
+    Private Sub btnEqual_Click(sender As Object, e As EventArgs) Handles btnEqual.Click
+        ' Pisahkan teks berdasarkan spasi
+        Dim ekspresi As String() = txtDisplay.Text.Split(" "c)
+
+        ' Validasi bahwa teks memiliki format [angka pertama] [operator] [angka kedua]
+        If ekspresi.Length <> 3 Then
+            MessageBox.Show("Input tidak valid! Pastikan Kamu memasukkan angka Pertama, operator, dan angka kedua.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Exit Sub
+        End If
+
+        Dim nilaiKedua As Integer
+        Try
+            ' Parsing nilai kedua dari teks
+            nilaiKedua = Integer.Parse(ekspresi(2))
+        Catch ex As Exception
+            MessageBox.Show("Angka kedua tidak valid!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Exit Sub
+        End Try
+
+        Dim hasil As Integer
+
+        ' Operasikan berdasarkan operator
+        Select Case ekspresi(1)
+            Case "+"
+                hasil = nilaiPertama + nilaiKedua
+            Case "-"
+                hasil = nilaiPertama - nilaiKedua
+            Case "*"
+                hasil = nilaiPertama * nilaiKedua
+            Case "/"
+                If nilaiKedua = 0 Then
+                    MessageBox.Show("Tidak bisa membagi dengan nol!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                    Exit Sub
+                End If
+                hasil = nilaiPertama \ nilaiKedua ' Pembagian integer
+            Case Else
+                MessageBox.Show("Operator tidak valid!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                Exit Sub
+        End Select
+
+        ' Tampilkan hasil
+        txtDisplay.Text = hasil.ToString()
+        operatorMatematika = ""
+        operasiDilakukan = False
+    End Sub
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        ' Reset semua variabel dan teks tampilan
+        txtDisplay.Text = "0"
+        nilaiPertama = 0
+        operatorMatematika = ""
+        operasiDilakukan = False
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
+
